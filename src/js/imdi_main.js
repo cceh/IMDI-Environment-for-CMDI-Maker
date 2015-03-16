57a7684a9db91d27aa8ee712bc05fe4c703b076e
Sebastian Zimmer <sebastian.zimmer@uni-koeln.de>
2015-03-16 12:33:41 +0100

    Build 2.6

--- src/js/imdi_main.js
+++ src/js/imdi_main.js
@@ -22,7 +22,7 @@ var imdi_environment = (function(){
 	my.name = "imdi";
 	my.id = "imdi";
 	my.title = "IMDI";
-	my.version = "2.5";
+	my.version = "2.6";
 	
 	my.workflow = [];
 	my.languages = [];
