a07e2701bba2a38d6ac1a4ef52211fe31cb7bb03
Miguel Ramirez <miguel.ramirezpena@uni-koeln.de>
2019-02-27 00:33:24 +0100

    update version of imdi profile
    bugfix #16

--- src/js/imdi_main.js
+++ src/js/imdi_main.js
@@ -22,7 +22,7 @@ var imdi_environment = (function(){
 	my.name = "imdi";
 	my.id = "imdi";
 	my.title = "IMDI";
-	my.version = "2.10";
+	my.version = "2.10.1";
 	
 	my.workflow = [];
 	my.languages = [];
